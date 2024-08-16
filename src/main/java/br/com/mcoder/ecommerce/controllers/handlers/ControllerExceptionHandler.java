7c87f1f203ce7c7ae1cdb9c482eb101df680663d
marcelloCoder <deoliveiral767@gmail.com>
2024-08-16 15:39:27 -0300

    treating exceptions

--- src/main/java/br/com/mcoder/ecommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/mcoder/ecommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package br.com.mcoder.ecommerce.controllers.handlers;
 
 import br.com.mcoder.ecommerce.dto.CustomError;
+import br.com.mcoder.ecommerce.dto.ValidationError;
 import br.com.mcoder.ecommerce.services.exceptions.DatabaseException;
 import br.com.mcoder.ecommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request){
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError error = new ValidationError(Instant.now(), status.value(), "Dados invalidos", request.getRequestURI());
+        for (FieldError f: e.getBindingResult().getFieldErrors()){
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(error);
     }
 
