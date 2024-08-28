93dfbb1ab120dbbdcb3bfbadd7a4391c2939bfca
marcelloCoder <deoliveiral767@gmail.com>
2024-08-28 10:30:15 -0300

    find by id categories

--- src/main/java/br/com/mcoder/ecommerce/controllers/CategoryController.java
+++ src/main/java/br/com/mcoder/ecommerce/controllers/CategoryController.java
@@ -1,14 +1,18 @@
 package br.com.mcoder.ecommerce.controllers;
 
 import br.com.mcoder.ecommerce.dto.CategoryDTO;
+import br.com.mcoder.ecommerce.dto.ProductDTO;
+import br.com.mcoder.ecommerce.entities.Category;
 import br.com.mcoder.ecommerce.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -17,9 +21,23 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO categoryDTO = categoryService.findById(id);
+        return ResponseEntity.ok(categoryDTO);
+    }
+
     @GetMapping
     public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
         Page<CategoryDTO> categoryDTOS = categoryService.findAll(pageable);
         return ResponseEntity.ok(categoryDTOS);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@Valid @RequestBody CategoryDTO categoryDTO){
+        categoryDTO = categoryService.insert(categoryDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(categoryDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(categoryDTO);
     }
 }
--- src/main/java/br/com/mcoder/ecommerce/services/CategoryService.java
+++ src/main/java/br/com/mcoder/ecommerce/services/CategoryService.java
@@ -1,8 +1,11 @@
 package br.com.mcoder.ecommerce.services;
 
 import br.com.mcoder.ecommerce.dto.CategoryDTO;
+import br.com.mcoder.ecommerce.dto.ProductDTO;
 import br.com.mcoder.ecommerce.entities.Category;
+import br.com.mcoder.ecommerce.entities.Product;
 import br.com.mcoder.ecommerce.repositories.CategoryRepository;
+import br.com.mcoder.ecommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,9 +19,28 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Category category = categoryRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new CategoryDTO(category);
+    }
+
+    @Transactional(readOnly = true)
     public Page<CategoryDTO> findAll(Pageable pageable){
         Page<Category> categoryPage = categoryRepository.findAll(pageable);
         return categoryPage.map(x -> new CategoryDTO(x));
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        copyDtoEntity(categoryDTO, category);
+        category = categoryRepository.save(category);
+        return new CategoryDTO(category);
+    }
+
+    private void copyDtoEntity(CategoryDTO categoryDTO, Category category){
+        category.setName(categoryDTO.getName());
     }
 
 }
