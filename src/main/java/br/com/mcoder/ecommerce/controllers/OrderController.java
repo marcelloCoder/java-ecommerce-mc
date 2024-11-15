941f7c3d5455317da2f7756e7732ad2fb34d7dd5
marcelloCoder <deoliveiral767@gmail.com>
2024-11-15 13:56:32 -0300

    Salvando pedido

--- src/main/java/br/com/mcoder/ecommerce/controllers/OrderController.java
+++ src/main/java/br/com/mcoder/ecommerce/controllers/OrderController.java
@@ -1,14 +1,9 @@
 package br.com.mcoder.ecommerce.controllers;
 
 import br.com.mcoder.ecommerce.dto.OrderDTO;
-import br.com.mcoder.ecommerce.dto.ProductDTO;
-import br.com.mcoder.ecommerce.dto.ProductMinDTO;
 import br.com.mcoder.ecommerce.services.OrderService;
-import br.com.mcoder.ecommerce.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +20,14 @@ public class OrderController {
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/{id}")
-    public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
+    public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = productService.findById(id);
-       return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(dto);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @PostMapping
-    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO orderDTO){
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO orderDTO) {
         orderDTO = productService.insert(orderDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(orderDTO.getId()).toUri();
--- src/main/java/br/com/mcoder/ecommerce/dto/OrderItemDTO.java
+++ src/main/java/br/com/mcoder/ecommerce/dto/OrderItemDTO.java
@@ -7,12 +7,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem orderItem) {
@@ -20,6 +22,7 @@ public class OrderItemDTO {
         name = orderItem.getProduct().getName();
         price = orderItem.getPrice();
         quantity = orderItem.getQuantity();
+        imgUrl = orderItem.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -37,6 +40,10 @@ public class OrderItemDTO {
 
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     public Double getSubTotal(){
